78a21c4ffc286f23929264bbbb16fdd633e530eb
noe.marx <noejadenmarx@gmail.com>
2026-05-26 07:54:41 +0200

    api key in image store

--- backend/src/backend/app/dependencies.py
+++ backend/src/backend/app/dependencies.py
@@ -1,9 +1,11 @@
 from fastapi.security import HTTPBearer, HTTPAuthorizationCredentials
-from fastapi import HTTPException, status, Depends
+from fastapi import HTTPException, status, Depends, Header
 from sqlalchemy.orm import Session
 from backend.app.user.model import User, Identity
 from backend.app.db.database import SessionLocal
 from backend.app.core.auth import verify_token
+from backend.app.apikey.model import Apikey
+from backend.app.core.security import decode_apikey, compare_hash
 
 security = HTTPBearer()
 
@@ -18,13 +20,13 @@ def get_current_user(credentials: HTTPAuthorizationCredentials = Depends(securit
     try:
         creds: dict = verify_token(credentials.credentials)
         iss = creds.get("iss")
-        if iss == None:
+        if iss is None:
             raise HTTPException(
                 status_code=status.HTTP_401_UNAUTHORIZED,
                 detail="Invalid token: missing issuer"
             )
         sub = creds.get("sub")
-        if sub == None:
+        if sub is None:
             raise HTTPException(
                 status_code=status.HTTP_401_UNAUTHORIZED,
                 detail="Invalid token: missing subject"
@@ -48,3 +50,23 @@ def get_current_user(credentials: HTTPAuthorizationCredentials = Depends(securit
             status_code=status.HTTP_401_UNAUTHORIZED,
             detail="Invalid token"
         ) from e
+
+def get_apikey(apikey: str = Header(None), db: Session = Depends(get_db)) -> Apikey:
+    creds = decode_apikey(apikey)
+
+    db_apikey = db.query(Apikey).filter(Apikey.prefix == creds.prefix).first()
+
+    if db_apikey is None:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Invalid apikey!"
+        )
+    
+    if not compare_hash(creds.secret, db_apikey.secret):
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Invalid apikey!"
+        )
+
+    return db_apikey
+    
---
+++ backend/src/backend/app/imagestore/service.py
@@ -0,0 +1,4 @@
+from backend.app.core.imagestore import add_image
+
+def upload_image_service():
+    stored_image = add_image()
